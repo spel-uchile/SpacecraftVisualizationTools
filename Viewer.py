af3da7799fa780e5bd39a4ab5e45af54feb142b4
crojas <camrojas@uchile.cl>
2020-01-30 04:30:06 -0300

    Made some refactoring in variables names and software flow

--- Viewer.py
+++ Viewer.py
@@ -21,13 +21,14 @@ from pyquaternion import Quaternion
 class Viewer(GeoDef, Qt.QMainWindow):
 
     def __init__(self, parent=None, show=True):
-        self.time_speed     = 1
-        self.earth_av       = 7.2921150*360.0*1e-5/(2*np.pi)
-        self.run_flag       = False
-        self.pause_flag     = False
-        self.stop_flag      = False
-        self.thread         = None
-        self.countTime      = 0
+        self.time_speed = 1
+        self.earth_av = 7.2921150*360.0*1e-5/(2*np.pi)
+        self.run_flag = False
+        self.pause_flag = False
+        self.stop_flag = False
+        self.thread = None
+        self.countTime = 0
+        self.screen = None
 
         Qt.QMainWindow.__init__(self, parent)
         # create the frame
@@ -48,66 +49,66 @@ class Viewer(GeoDef, Qt.QMainWindow):
         self.add_bar()
         self.add_i_frame_attitude()
 
-        #--------------------------------------------------------------------------------------------------------------
+        # --------------------------------------------------------------------------------------------------------------
         # simple menu to functions
-        mainMenu = self.menuBar()
-        #--------------------------------------------------------------------------------------------------------------
+        main_menu = self.menuBar()
+        # --------------------------------------------------------------------------------------------------------------
         # File option
-        fileMenu        = mainMenu.addMenu('File')
-        loadcsvData     = Qt.QAction('Load csv data', self)
-        exitButton      = Qt.QAction('Exit', self)
+        fileMenu = main_menu.addMenu('File')
+        loadcsvData = Qt.QAction('Load csv data', self)
+        exitButton = Qt.QAction('Exit', self)
 
         exitButton.setShortcut('Ctrl+Q')
         loadcsvData.triggered.connect(self.load_csv_file)
         exitButton.triggered.connect(self.close)
         fileMenu.addAction(loadcsvData)
         fileMenu.addAction(exitButton)
-        #--------------------------------------------------------------------------------------------------------------
+        # --------------------------------------------------------------------------------------------------------------
         # Path Orbit option
-        OrbMenu         = mainMenu.addMenu('Orbit')
-        orbit_action    = Qt.QAction('Show Orbit', self)
-        cad_action      = Qt.QAction('Add satellite', self)
-        aries_action    = Qt.QAction('Add vector to Vernal Equinox', self)
+        orb_menu = main_menu.addMenu('Orbit')
+        # orbit_action    = Qt.QAction('Show Orbit', self)
+        # cad_action      = Qt.QAction('Add satellite', self)
+        aries_action = Qt.QAction('Add vector to Vernal Equinox', self)
 
-        orbit_action.triggered.connect(self.add_orbit)
-        cad_action.triggered.connect(self.add_spacecraft_2_orbit)
+        # orbit_action.triggered.connect(self.add_orbit)
+        # cad_action.triggered.connect(self.add_spacecraft_2_orbit)
         aries_action.triggered.connect(self.add_aries_arrow)
 
-        OrbMenu.addAction(orbit_action)
-        OrbMenu.addAction(cad_action)
-        OrbMenu.addAction(aries_action)
-        #--------------------------------------------------------------------------------------------------------------
+        # orb_menu.addAction(orbit_action)
+        # orb_menu.addAction(cad_action)
+        orb_menu.addAction(aries_action)
+        # --------------------------------------------------------------------------------------------------------------
         # Attitude
-        AttMenu         = mainMenu.addMenu('Attitude')
-        sat_action      = Qt.QAction('Add satellite', self)
-        frame_action    = Qt.QAction('Add Body reference frame', self)
-
-        sat_action.triggered.connect(self.add_spacecraft_2_attitude)
-        AttMenu.addAction(sat_action)
-        frame_action.triggered.connect(self.add_b_frame_attitude)
-        AttMenu.addAction(frame_action)
-        #--------------------------------------------------------------------------------------------------------------
+        # AttMenu         = main_menu.addMenu('Attitude')
+        # sat_action      = Qt.QAction('Add satellite', self)
+        # frame_action    = Qt.QAction('Add Body reference frame', self)
+        #
+        # sat_action.triggered.connect(self.add_spacecraft_2_attitude)
+        # frame_action.triggered.connect(self.add_b_frame_attitude)
+        # AttMenu.addAction(sat_action)
+        # AttMenu.addAction(frame_action)
+        # --------------------------------------------------------------------------------------------------------------
         # Simulation option
-        SimMenu         = mainMenu.addMenu('Simulation')
-        run_action      = Qt.QAction('Run', self)
-        pause_action    = Qt.QAction('Pause', self)
-        stop_action     = Qt.QAction('Stop', self)
+        sim_menu = main_menu.addMenu('Simulation')
+        run_action = Qt.QAction('Run', self)
+        pause_action = Qt.QAction('Pause', self)
+        stop_action = Qt.QAction('Stop', self)
 
         run_action.triggered.connect(self.run_simulation)
         pause_action.triggered.connect(self.pause_simulation)
         stop_action.triggered.connect(self.stop_simulation)
 
-        SimMenu.addAction(run_action)
-        SimMenu.addAction(pause_action)
-        SimMenu.addAction(stop_action)
-        #--------------------------------------------------------------------------------------------------------------
-        graph2dMenu = mainMenu.addMenu('Data')
+        sim_menu.addAction(run_action)
+        sim_menu.addAction(pause_action)
+        sim_menu.addAction(stop_action)
+        # --------------------------------------------------------------------------------------------------------------
+        graph2d_menu = main_menu.addMenu('Data')
         plot_action = Qt.QAction('Generate graph', self)
 
         plot_action.triggered.connect(self.add_graph2d)
 
-        graph2dMenu.addAction(plot_action)
-        #--------------------------------------------------------------------------------------------------------------
+        graph2d_menu.addAction(plot_action)
+        # --------------------------------------------------------------------------------------------------------------
 
         if show:
             self.show()
@@ -130,6 +131,16 @@ class Viewer(GeoDef, Qt.QMainWindow):
             dataLog = read_data(filename)
             self.datalog = DataHandler(dataLog)
             self.datalog.create_variable()
+
+            # Add actors to orbit view
+            self.add_orbit()
+            self.add_spacecraft_2_orbit()
+            self.add_aries_arrow()
+
+            # Add actors to attitude view
+            self.add_spacecraft_2_attitude()
+            self.add_b_frame_attitude()
+
             print('Data log created')
         else:
             print('Could not create data log')
@@ -147,50 +158,53 @@ class Viewer(GeoDef, Qt.QMainWindow):
     def stop_simulation(self):
         return
 
-    def resetTime(self):
+    def reset_time(self):
         self.countTime = 0
 
-    def updateTime(self):
+    def update_time(self):
         self.countTime += self.datalog.stepTime
 
+    def update_meshes(self, index):
+        # Update Earth
+        self.sphere.rotate_z(10)
+
+        # Update Orbit
+        self.current_pos = self.datalog.sat_pos_i[index, :] - self.datalog.sat_pos_i[index - 1, :]
+        self.spacecraft_in_orbit.translate(self.current_pos)
+
+        # Update Attitude
+        self.update_attitude(index)
+
+        # Update widget
+        self.vtk_widget.update()
+
     def rotate_th(self):
-        self.vtk_widget.subplot(0,0)
+        self.vtk_widget.subplot(0, 0)
         i = 1
-        self.resetTime()
+        self.reset_time()
         while self.countTime < self.datalog.endTime:
+
             while self.pause_flag:
                 if self.run_flag:
                     self.pause_flag = False
 
-            # Update Earth
-            self.sphere.rotate_z(10)
-
-            # Update Orbit
-            self.current_pos = self.datalog.sat_pos_i[i, :] - self.datalog.sat_pos_i[i - 1, :]
-            self.spacecraft_in_orbit.translate(self.current_pos)
-
-            # Update Attitude
-            self.updateAttitude(i)
-
-            # Update widget
-            self.vtk_widget.update()
-            time.sleep(self.datalog.stepTime/self.time_speed)
+            self.update_meshes(i)
+            time.sleep(self.datalog.stepTime / self.time_speed)
 
             # Update time
-            self.updateTime()
+            self.update_time()
             i += 1
 
-
     def run_orbit_3d(self):
-        if self.thread == None:
-            self.thread = Thread(target = self.rotate_th, daemon = True)
+        if self.thread is None:
+            self.thread = Thread(target=self.rotate_th, daemon=True)
             self.thread.start()
 
     def sim_speed(self, value):
         self.time_speed = value
         return
 
-    def updateAttitude(self, i):
+    def update_attitude(self, i):
         quaternion_ti = Quaternion(self.datalog.q_t_i2b[i, :])
         inv_quaternion = self.quaternion_t0.inverse
         d_quaternion = inv_quaternion*quaternion_ti
